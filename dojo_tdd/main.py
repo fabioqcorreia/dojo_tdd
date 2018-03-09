a86c9b640d3a8aeff7fb602581c65e48d701e9b7
fabio.correia <fabio.correia@accenture.com>
2018-03-09 11:01:48 -0300

    feat: Add sum and subtract
    Add new features.

---
+++ dojo_tdd/main.py
@@ -0,0 +1,8 @@
+
+class Calculator():
+
+    def sum(self, first_value, second_value):
+        return first_value + second_value
+
+    def subtract(self, first_value, second_value):
+        return first_value - second_value
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,9 +1,29 @@
 import unittest
-
+from dojo_tdd.main import Calculator
 
 class UnitTest(unittest.TestCase):
     """
     Os testes devem começar com "test_".
     """
-    def test_case(self, val1=2, val2=3):
-        self.assertEqual(val1+val2, 5)
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def tearDown(self):
+        pass
+
+    def test_can_sum_two_integers(self):
+
+        # Act
+        response = self.calculator.sum(1, 1)
+
+        # Assert
+        self.assertEqual(2, response)
+
+    def test_can_subtract_two_integers(self):
+
+        # Act
+        response = self.calculator.subtract(2,1)
+
+        # Assert
+        self.assertEqual(1, response)
